db99370cc6bc9412aaf1625a59f8fe8bb9e098ea
kmax.eth <92766553+maxkaneco@users.noreply.github.com>
2022-12-13 19:22:01 +0800

    upgrade ipfs dependency version (#1)
    * upgrade ipfs dependency version
    
    * fix workflow
    
    * remove dependency on go-photon

--- config.go
+++ config.go
@@ -2,17 +2,18 @@ package ipfs
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-datastore"
 	dssync "github.com/ipfs/go-datastore/sync"
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
 	"github.com/ipfs/go-merkledag"
-	"github.com/ipfs/kubo/config"
-	"github.com/ipfs/kubo/thirdparty/verifbs"
-	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/ipfs/go-verifcid"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/multiformats/go-multiaddr"
 )
 
@@ -56,7 +57,7 @@ func fillConfigDefaults(ctx context.Context, c Config) (Config, error) {
 	}
 
 	if len(c.Bootstrappers) == 0 {
-		bootstrappers, err := config.DefaultBootstrapPeers()
+		bootstrappers, err := defaultBootstrapPeers()
 		if err != nil {
 			return Config{}, err
 		}
@@ -80,7 +81,7 @@ func fillConfigDefaults(ctx context.Context, c Config) (Config, error) {
 	if c.Blockstore == nil {
 		bs, err := blockstore.CachedBlockstore(
 			ctx,
-			&verifbs.VerifBS{
+			&VerifBS{
 				Blockstore: blockstore.NewBlockstore(c.Datastore),
 			},
 			blockstore.DefaultCacheOpts(),
@@ -100,4 +101,72 @@ func fillConfigDefaults(ctx context.Context, c Config) (Config, error) {
 	}
 
 	return c, nil
+}
+
+// The following code is copied from "github.com/ipfs/kubo/config" to avoid
+// dependency on kubo package which introduces conflicts on libp2p-core.
+// DefaultBootstrapAddresses are the hardcoded bootstrap addresses
+// for IPFS. they are nodes run by the IPFS team. docs on these later.
+// As with all p2p networks, bootstrap is an important security concern.
+var defaultBootstrapAddresses = []string{
+	"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+	"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+	"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+	"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+	"/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",      // mars.i.ipfs.io
+	"/ip4/104.131.131.82/udp/4001/quic/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ", // mars.i.ipfs.io
+}
+
+// defaultBootstrapPeers returns the (parsed) set of default bootstrap peers.
+// if it fails, it returns a meaningful error for the user.
+// This is here (and not inside cmd/ipfs/init) because of module dependency problems.
+func defaultBootstrapPeers() ([]peer.AddrInfo, error) {
+	ps, err := parseBootstrapPeers(defaultBootstrapAddresses)
+	if err != nil {
+		return nil, fmt.Errorf(`failed to parse hardcoded bootstrap peers: %s
+This is a problem with the ipfs codebase. Please report it to the dev team`, err)
+	}
+	return ps, nil
+}
+
+// ParseBootstrapPeer parses a bootstrap list into a list of AddrInfos.
+func parseBootstrapPeers(addrs []string) ([]peer.AddrInfo, error) {
+	maddrs := make([]multiaddr.Multiaddr, len(addrs))
+	for i, addr := range addrs {
+		var err error
+		maddrs[i], err = multiaddr.NewMultiaddr(addr)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return peer.AddrInfosFromP2pAddrs(maddrs...)
+}
+
+// Copied from github.com/ipfs/kubo/thirdparty/verifbs to avoid dependency on
+// Kubo, which introduces version conflicts for libp2p-core
+type VerifBS struct {
+	blockstore.Blockstore
+}
+
+func (bs *VerifBS) Put(ctx context.Context, b blocks.Block) error {
+	if err := verifcid.ValidateCid(b.Cid()); err != nil {
+		return err
+	}
+	return bs.Blockstore.Put(ctx, b)
+}
+
+func (bs *VerifBS) PutMany(ctx context.Context, blks []blocks.Block) error {
+	for _, b := range blks {
+		if err := verifcid.ValidateCid(b.Cid()); err != nil {
+			return err
+		}
+	}
+	return bs.Blockstore.PutMany(ctx, blks)
+}
+
+func (bs *VerifBS) Get(ctx context.Context, c cid.Cid) (blocks.Block, error) {
+	if err := verifcid.ValidateCid(c); err != nil {
+		return nil, err
+	}
+	return bs.Blockstore.Get(ctx, c)
 }
